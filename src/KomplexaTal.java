a46818679495a4841a65e430b4d93b212d596ed7
sumsar8 <rasmus.oberg@elev.ga.ntig.se>
2021-02-26 11:15:27 +0100

    En början

---
+++ src/KomplexaTal.java
@@ -0,0 +1,9 @@
+import KomplexaTal.java.Komplex;
+
+public class KomplexaTal {
+    public static void main(String[]args) {
+
+        Komplex a = new Komplex(10,5);
+        System.out.println("talet borde vara 10 + 5i:\n" + i1);
+    }
+}
---
+++ src/KomplexaTal/java/Komplex.java
@@ -0,0 +1,7 @@
+package KomplexaTal.java;
+
+public class Komplex {
+    public Komplex(int i1, int i2) {
+
+    }
+}
